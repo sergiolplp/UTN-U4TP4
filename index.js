7a8f8ddba660a1061193bd1dc87544431a1544e0
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-02 19:40:11 -0300

    nuevos archivos

---
+++ index.js
@@ -0,0 +1,21 @@
+// var precio, descuento, totaldescuento, totalacobrar
+
+// precio=parseFloat(prompt ("Indique el precio"));
+// descuento=parseFloat(prompt("Indique % de descuento"));
+
+// totaldescuento = precio*descuento/100
+// totalacobrar = precio-totaldescuento;
+
+// alert ("El descuento es de: " + totaldescuento);
+// alert ("El total a cobrar es de: " + totalacobrar);
+// document.write("El descuento es de: " + totaldescuento);
+// document.write("<br><br>El total a cobrar es de: " + totalacobrar);
+
+let metros = prompt("Por favor, indique la cantidad de metros y le aconsejaré que medio de transporte utilizar");
+
+if(metros<=1000){alert("Le recomiendo ir a pié");
+}
+else if (metros<=10000){alert("Debería subir a una bicicleta");}
+else if (metros<=30000){alert("Debería tomar un colectivo");}
+else if (metros<=100000){alert("Tome un taxi o remisse");}
+else (metros>=10000);{alert("Lo mejor sería viajar en avión");}
