e15b5a74a2a816ae0e5b14bc5cf517c794e48bd9
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-02 20:16:50 -0300

    Arreglos

--- index.js
+++ index.js
@@ -11,11 +11,11 @@
 // document.write("El descuento es de: " + totaldescuento);
 // document.write("<br><br>El total a cobrar es de: " + totalacobrar);
 
-let metros = prompt("Por favor, indique la cantidad de metros y le aconsejaré que medio de transporte utilizar");
+// let metros = prompt("Por favor, indique la cantidad de metros y le aconsejaré que medio de transporte utilizar");
 
-if(metros<=1000){alert("Le recomiendo ir a pié");
-}
-else if (metros<=10000){alert("Debería subir a una bicicleta");}
-else if (metros<=30000){alert("Debería tomar un colectivo");}
-else if (metros<=100000){alert("Tome un taxi o remisse");}
-else (metros>=10000);{alert("Lo mejor sería viajar en avión");}
+// if(metros<=1000){alert("Le recomiendo ir a pié");
+// }
+// else if (metros<=10000){alert("Debería subir a una bicicleta");}
+// else if (metros<=30000){alert("Debería tomar un colectivo");}
+// else if (metros<=100000){alert("Tome un taxi o remisse");}
+// else (metros>=10000);{alert("Lo mejor sería viajar en avión");}
