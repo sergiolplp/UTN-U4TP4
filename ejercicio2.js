69eef1698d46a90f87278403a694582a4e547b88
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-04 17:32:47 -0300

    Arreglos

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+// Profesora. No supe resolver este ejercicio hasta que googleando entendí que los valores que ingresa un usuario
+// con el input prompt es un String aún siendo números. Por lo que para que JS entienda que son números, se debe utilizar
+// parseInt.
+
+var nombre = prompt ('Hola, ¿como te llamas?');
+alert ('Hola '+nombre+' escribe tres números y te diré cuál es el mayor. ¿Empezamos?');
+
+var valor1 = parseInt (prompt('Escribe el primer número.'));
+var valor2 = parseInt (prompt('Muy bien! Escribe ahora el segundo.'));
+var valor3 = parseInt (prompt('Perfecto! ya casi terminamos, escribe ahora el último número.'));
+
+if (valor1>=valor2 && valor1>=valor3){document.write('el número mayor es '+valor1);}
+else if (valor2>=valor1 && valor2>=valor3){document.write('el número mayor es '+valor2);}
+else document.write('el número mayor es '+valor3);
