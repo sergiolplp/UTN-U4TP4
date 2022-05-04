debaadbd025b56ab8653a752c9f65062b37313a5
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-04 17:51:01 -0300

    lutimos arreglos

--- ejercicio2.js
+++ ejercicio2.js
@@ -9,6 +9,6 @@ var valor1 = parseInt (prompt('Escribe el primer número.'));
 var valor2 = parseInt (prompt('Muy bien! Escribe ahora el segundo.'));
 var valor3 = parseInt (prompt('Perfecto! ya casi terminamos, escribe ahora el último número.'));
 
-if (valor1>=valor2 && valor1>=valor3){document.write('el número mayor es '+valor1);}
-else if (valor2>=valor1 && valor2>=valor3){document.write('el número mayor es '+valor2);}
-else document.write('el número mayor es '+valor3);
+if (valor1>=valor2 && valor1>=valor3){document.write('<h2>el número mayor es '+valor1+'</h2>');}
+else if (valor2>=valor1 && valor2>=valor3){document.write('<h2>el número mayor es '+valor2+'</h2>');}
+else document.write('<h2>el número mayor es '+valor3+'</h2>');
