764b1b0a6e2837ac113dd9d9eb33a47f36e04095
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-05-04 19:37:58 -0300

    Ejercicio 2.1

---
+++ ejercicio2.1.js
@@ -0,0 +1,14 @@
+var nombre = prompt ('Hola, ¿como te llamas?');
+alert ('Hola '+nombre+' escribe tres números y te diré cuál es el mayor. ¿Empezamos?');
+
+var valor1 = parseInt (prompt('Escribe el primer número.'));
+var valor2 = parseInt (prompt('Muy bien! Escribe ahora el segundo.'));
+var valor3 = parseInt (prompt('Perfecto! ya casi terminamos, escribe ahora el último número.'));
+
+var numeros = (valor1, valor2, valor3);
+var i = 0;
+
+for(var i=0; i<numeros.length; i++) 
+if(numeros > mayor){mayor - numeros};
+
+document.write('<h2>el número mayor es '+mayor+'</h2>');
